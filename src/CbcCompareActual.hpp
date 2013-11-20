f50a82d3dd9559486f0f6ce4112ec6917705a502
Tony Kelman <tony@kelman.net>
2013-11-20 01:47:54 -0800

    Squashed 'Cbc/' content from commit 17d46d4
    git-subtree-dir: Cbc
    git-subtree-split: 17d46d468960611a0dd536f083742e225f258d64

---
+++ src/CbcCompareActual.hpp
@@ -0,0 +1,12 @@
+/* $Id$ */
+// Copyright (C) 2002, International Business Machines
+// Corporation and others.  All Rights Reserved.
+#ifndef CbcCompareActual_H
+#define CbcCompareActual_H
+#include "CbcNode.hpp"
+#include "CbcCompareBase.hpp"
+#include "CbcCompare.hpp"
+#include "CbcCompareDepth.hpp"
+#include "CbcCompareDefault.hpp"
+#endif
+
