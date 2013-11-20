06b846e814407f549cc64da3d62826b47603a291
Tony Kelman <tony@kelman.net>
2013-11-20 01:36:22 -0800

    Squashed 'Clp/' content from commit 62194f9
    git-subtree-dir: Clp
    git-subtree-split: 62194f9695ef435f24a3682cbaf9ba319555d2c8

---
+++ examples/minimum.cpp
@@ -0,0 +1,23 @@
+/* $Id$ */
+// Copyright (C) 2002, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+#include "ClpSimplex.hpp"
+int main(int argc, const char *argv[])
+{
+     ClpSimplex  model;
+     int status;
+     if (argc < 2) {
+#if defined(SAMPLEDIR)
+          status = model.readMps(SAMPLEDIR "/p0033.mps", true);
+#else
+          fprintf(stderr, "Do not know where to find sample MPS files.\n");
+          exit(1);
+#endif
+     } else
+          status = model.readMps(argv[1]);
+     if (!status) {
+          model.primal();
+     }
+     return 0;
+}
