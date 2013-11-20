775acccb51f8e3e2fd65eacc75a1771f8b8a6aa3
Tony Kelman <tony@kelman.net>
2013-11-20 02:27:30 -0800

    Squashed 'Couenne/' content from commit 968f66f
    git-subtree-dir: Couenne
    git-subtree-split: 968f66f91b049fe223bcf7b80034aaa5596f968e

---
+++ src/cut/sdpcuts/CouenneSdpCuts.hpp
@@ -0,0 +1,17 @@
+/* $Id$
+ *
+ * Name:    CouenneSdpCuts
+ * Author:  Pietro Belotti
+ * Purpose: wrapper for Couenne to insert sdpcuts
+ *
+ * This file is licensed under the Eclipse Public License (EPL)
+ */
+
+#ifndef CouenneSdpCuts_hpp
+#define CouenneSdpCuts_hpp
+
+namespace Couenne {
+
+}
+
+#endif
