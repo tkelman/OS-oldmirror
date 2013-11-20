2819ae2c2308fec57f31dc7db09a50a8bd450065
Tony Kelman <tony@kelman.net>
2013-11-20 02:00:29 -0800

    Squashed 'Bcp/' content from commit c013c94
    git-subtree-dir: Bcp
    git-subtree-split: c013c94c2e6a31c132fdb75c2b32f566b226c1d1

---
+++ src/include/config_bcp_default.h
@@ -0,0 +1,20 @@
+/***************************************************************************/
+/*           HERE DEFINE THE PROJECT SPECIFIC PUBLIC MACROS                */
+/*    These are only in effect in a setting that doesn't use configure     */
+/***************************************************************************/
+
+/* Version number of project */
+#define BCP_VERSION "1.3.1"
+
+/* Major Version number of project */
+#define BCP_VERSION_MAJOR 1
+
+/* Minor Version number of project */
+#define BCP_VERSION_MINOR 3
+
+/* Release Version number of project */
+#define BCP_VERSION_RELEASE 1
+
+/* Define to 1 if the Mpi package is used */
+/* #define COIN_HAS_MPI 1 */
+
