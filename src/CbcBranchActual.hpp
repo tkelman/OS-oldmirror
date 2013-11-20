cc677bf7604efb1ebbf2c7fb5fba90f44ffa206b
Tony Kelman <tony@kelman.net>
2013-11-20 01:36:10 -0800

    Squashed 'Cbc/' content from commit a51d47e
    git-subtree-dir: Cbc
    git-subtree-split: a51d47ebb0c04f8b437f53d839b55cc7daad3731

---
+++ src/CbcBranchActual.hpp
@@ -0,0 +1,22 @@
+/* $Id$ */
+// Copyright (C) 2002, International Business Machines
+// Corporation and others.  All Rights Reserved.
+#ifndef CbcBranchActual_H
+#define CbcBranchActual_H
+
+#include "CbcBranchBase.hpp"
+#include "CoinPackedMatrix.hpp"
+#include "CbcClique.hpp"
+#include "CbcSOS.hpp"
+#include "CbcSimpleInteger.hpp"
+#include "CbcNWay.hpp"
+#include "CbcSimpleIntegerPseudoCost.hpp"
+#include "CbcBranchDefaultDecision.hpp"
+#include "CbcFollowOn.hpp"
+#include "CbcFixVariable.hpp"
+#include "CbcDummyBranchingObject.hpp"
+#include "CbcGeneral.hpp"
+#include "CbcGeneralDepth.hpp"
+#include "CbcSubProblem.hpp"
+#endif
+
